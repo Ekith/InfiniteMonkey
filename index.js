955dcda262e8e15c535c031593181c66a7ed9f47
Ekith <ekith-games@outlook.fr>
2025-04-08 14:02:28 +0200

    Pimp my page d;

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ window.addEventListener("DOMContentLoaded",init)
 
 let monkey = ""
 let maxChar = 0
+let pause = false
 
 function generateRandomChar(alphabet) {
     let n = alphabet.length;
     let rdm = getRandomInt(n);
     return alphabet[rdm];
 }
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
@@ -28,14 +30,15 @@ function writeFile(filename, data) {
 
 
 function writePage(text) {
-    if (maxChar > 0) {
-        let len = text.length;
-        document.getElementById("monkey").innerHTML = text.substring(len - maxChar, len)
-    }
-    else {
-        document.getElementById("monkey").innerHTML = text;
-    }
-    document.getElementById("nbChar").innerHTML = document.getElementById("monkey").innerHTML.length;
+    if (pause == false)
+        if (maxChar > 0) {
+            let len = text.length;
+            document.getElementById("monkey").innerHTML = text.substring(len - maxChar, len)
+        }
+        else {
+            document.getElementById("monkey").innerHTML = text;
+        }
+    document.getElementById("nbChar").innerHTML = text.length;
 }
 
 function searchInStr(part) {
@@ -69,20 +72,51 @@ function init() {
 
     let interval1 = window.setInterval(addChar, 10);
 
+    if (pause) {
+        document.getElementById("pauseButton").innerHTML = "Pause";
+    }
+    else {
+        document.getElementById("pauseButton").innerHTML = "Play";
+    }
 
-    document.getElementById("stopButton").addEventListener("click", function () {
-        window.clearInterval(interval1);
+
+    document.getElementById("pauseButton").addEventListener("click", function () {
+        pause = !pause;
+        if (pause) {
+            document.getElementById("pauseButton").innerHTML = "Pause";
+        }
+        else {
+            document.getElementById("pauseButton").innerHTML = "Play";
+        }
     });
     document.getElementById("searchButton").addEventListener("click", function () {
        searchInStr(document.getElementById("searchInput").value);
     });
     document.getElementById("all").addEventListener("click", function () {
+        document.getElementById("nbWords").innerHTML = "All";
+
+        document.getElementById("all").disabled = true;
+        document.getElementById("1000").disabled = false;
+        document.getElementById("100").disabled = false;
+
         maxChar = 0
     });
     document.getElementById("1000").addEventListener("click", function () {
+        document.getElementById("nbWords").innerHTML = "1000";
+
+        document.getElementById("all").disabled = false;
+        document.getElementById("1000").disabled = true;
+        document.getElementById("100").disabled = false;
+
         maxChar = 1000
     });
     document.getElementById("100").addEventListener("click", function () {
+        document.getElementById("nbWords").innerHTML = "100";
+
+        document.getElementById("all").disabled = false;
+        document.getElementById("1000").disabled = false;
+        document.getElementById("100").disabled = true;
+
         maxChar = 100
     });
 
