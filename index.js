c0dab0ca16c6ea5d175104bfc5af13c46d4e0f97
Ekith <ekith-games@outlook.fr>
2025-04-08 12:28:30 +0200

    Change on html

--- index.js
+++ index.js
@@ -3,31 +3,38 @@ const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
 
 window.addEventListener("DOMContentLoaded",init)
 
+let monkey = ""
+let maxChar = 0
 
 function generateRandomChar(alphabet) {
     let n = alphabet.length;
     let rdm = getRandomInt(n);
-
     return alphabet[rdm];
 }
-
-
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
+
+
 
 // return the content of the file give in argument
 function readFile(filename) {
     return 'Gnagnagna'
 }
-
+// write data into a github file
 function writeFile(filename, data) {
 
 }
 
-function writePage() {
-    let letter = generateRandomChar(alphabet);
-    document.getElementById("monkey").innerHTML += letter;
+
+function writePage(text) {
+    if (maxChar > 0) {
+        let len = text.length;
+        document.getElementById("monkey").innerHTML = text.substring(len - maxChar, len)
+    }
+    else {
+        document.getElementById("monkey").innerHTML = text;
+    }
     document.getElementById("nbChar").innerHTML = document.getElementById("monkey").innerHTML.length;
 }
 
@@ -47,19 +54,37 @@ function searchInStr(part) {
 }
 
 
+function addChar() {
+    let newChar = generateRandomChar(alphabet);
+    monkey += newChar;
+    writePage(monkey);
+    writeFile("monkey.txt", newChar);
+}
+
+
 function init() {
 
 
     console.log("2 " + readFile("monkey.txt"));
 
-    let interval1 = window.setInterval(writePage, 1);
+    let interval1 = window.setInterval(addChar, 10);
 
-    document.getElementById("resetButton").addEventListener("click", function () {
+
+    document.getElementById("stopButton").addEventListener("click", function () {
         window.clearInterval(interval1);
     });
-
     document.getElementById("searchButton").addEventListener("click", function () {
        searchInStr(document.getElementById("searchInput").value);
     });
+    document.getElementById("all").addEventListener("click", function () {
+        maxChar = 0
+    });
+    document.getElementById("1000").addEventListener("click", function () {
+        maxChar = 1000
+    });
+    document.getElementById("100").addEventListener("click", function () {
+        maxChar = 100
+    });
+
 
 }
